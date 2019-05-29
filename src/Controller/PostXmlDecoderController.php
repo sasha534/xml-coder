f9b6574e3b184e553330860240b52691a8fed61d
Олександр <42872895+sasha534@users.noreply.github.com>
2019-05-29 18:44:59 +0300

    form sending and data name getting

--- src/Controller/PostXmlDecoderController.php
+++ src/Controller/PostXmlDecoderController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -15,26 +16,27 @@ class PostXmlDecoderController extends AbstractController
      */
     public function contactAction(Request $request) {
         $form = $this->createFormBuilder()
-            ->add('name', TextType::class)
+            ->add('name', TextareaType::class)
             ->add('send', SubmitType::class, ['label' => 'Create Task'])
             ->getForm();
 
         $form->handleRequest($request);
 
         if($form->isSubmitted()) {
-            var_dump($request->request);
-            die(var_dump($request->request->get('name')));
+            //$name = $request->request->get('name');
+            return $this->render('post_xml_decoder/index.html.twig', array(
+                //'controller_name' => 'PostXmlDecoderController',
+                'form' => $form->createView(),
+                'name' => $request->request->get('form'),
+            ));
         }
 
 
 
-
-
-
-
-        return $this->render('post_xml_decoder/index.html.twig', [
+        return $this->render('post_xml_decoder/index.html.twig', array(
             //'controller_name' => 'PostXmlDecoderController',
             'form' => $form->createView(),
-        ]);
+            //'name' => $name,
+        ));
     }
 }
